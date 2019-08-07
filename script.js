4d9b7f1e1fef32391711fe0ff71c22b954d6caa9
a-leja <41519670+a-leja@users.noreply.github.com>
2019-08-07 19:26:49 +0200

    amend ApiRoot

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://whispering-bastion-50699.herokuapp.com/v1/tasks/';
+  const apiRoot = 'https://whispering-bastion-50699.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://whispering-bastion-50699.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
