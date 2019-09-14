2195f5a5175edda33dd162a3f411e367390e9de1
a-leja <41519670+a-leja@users.noreply.github.com>
2019-09-14 20:11:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://whispering-bastion-50699.herokuapp.com/v1';
+  const apiRoot = 'https://whispering-bastion-50699.herokuapp.com/v1/';
   const trelloApiRoot = 'https://whispering-bastion-50699.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
